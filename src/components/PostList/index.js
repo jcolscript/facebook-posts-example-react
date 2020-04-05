33cb3a9b8dfacd2e585c3b8270066000706ce25d
José Carlos <jcolscript@gmail.com>
2020-04-05 04:02:41 -0300

    ajustando o mock da lista de posts

--- src/components/PostList/index.js
+++ src/components/PostList/index.js
@@ -21,7 +21,7 @@ class PostList extends Component {
             id: 1,
             author: {
               name: "José Carlos",
-              avatar: "https://i.pravatar.cc/150?img=5"
+              avatar: "https://i.pravatar.cc/150?img=3"
             },
             content:
               "Pellentesque varius leo fringilla lacus convallis, quis ornare urna hendrerit."
@@ -32,7 +32,7 @@ class PostList extends Component {
         id: 2,
         author: {
           name: "Julio Alcantara",
-          avatar: "https://i.pravatar.cc/150?img=1"
+          avatar: "https://i.pravatar.cc/150?img=10"
         },
         date: "04 Jun 2019",
         content:
@@ -52,6 +52,36 @@ class PostList extends Component {
             author: {
               name: "José Carlos",
               avatar: "https://i.pravatar.cc/150?img=3"
+            },
+            content:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean bibendum eu risus nec scelerisque."
+          }
+        ]
+      },
+      {
+        id: 3,
+        author: {
+          name: "José Carlos",
+          avatar: "https://i.pravatar.cc/150?img=3"
+        },
+        date: "04 Jun 2019",
+        content:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean bibendum eu risus nec scelerisque.",
+        comments: [
+          {
+            id: 1,
+            author: {
+              name: "Microsoft",
+              avatar: "https://i.pravatar.cc/150?img=2"
+            },
+            content:
+              "Aenean vitae justo tellus. Nulla eleifend gravida purus sit amet commodo. Aenean nec eros risus. Donec posuere tincidunt lorem vitae maximus. Morbi fermentum erat ultrices lacus interdum, eget dictum lectus lobortis."
+          },
+          {
+            id: 2,
+            author: {
+              name: "Ricardo Gabriel",
+              avatar: "https://i.pravatar.cc/150?img=25"
             },
             content:
               "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean bibendum eu risus nec scelerisque."
