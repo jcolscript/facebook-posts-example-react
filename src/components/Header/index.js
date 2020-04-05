76b9295735373445d53427e2caf8aba9298e7c31
José Carlos <jcolscript@gmail.com>
2020-04-05 01:03:41 -0300

    comcluindo o header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,28 @@
 import React, { Component } from "react";
 
+import "./style.css";
+
+import logo from "../../assets/logo.svg";
+
 class Header extends Component {
   render() {
-    return <h1>Hello Word</h1>;
+    return (
+      <div id="nav-container">
+        <div className="nav_primary_placeholder">
+          <div className="nav nav_primary">
+            <div className="container nav__container">
+              <div className="brand .brand__logo">
+                <img width="150px" src={logo} alt="" />
+              </div>
+              <div className="userInfo">
+                <span>Meu perfil</span>
+                <span class="material-icons">account_circle</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
